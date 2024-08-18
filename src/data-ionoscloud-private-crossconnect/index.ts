621136cda0774aa6eac32ab5965a7d51701ae141
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-18 06:04:21 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-ionoscloud-private-crossconnect/index.ts
+++ src/data-ionoscloud-private-crossconnect/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/ionos-cloud/ionoscloud/6.5.0/docs/data-sources/private_crossconnect
 // generated from terraform resource schema
 
