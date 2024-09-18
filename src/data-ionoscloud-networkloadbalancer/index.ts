328074eb30c3ef60a24acdebf1b624e56b4d18e2
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-18 03:20:10 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-ionoscloud-networkloadbalancer/index.ts
+++ src/data-ionoscloud-networkloadbalancer/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/ionos-cloud/ionoscloud/6.5.5/docs/data-sources/networkloadbalancer
 // generated from terraform resource schema
 
