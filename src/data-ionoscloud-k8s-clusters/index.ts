387cf06e52e943a235891b9cc9d161326570c3cc
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-17 06:08:26 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-ionoscloud-k8s-clusters/index.ts
+++ src/data-ionoscloud-k8s-clusters/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/ionos-cloud/ionoscloud/6.6.2/docs/data-sources/k8s_clusters
 // generated from terraform resource schema
 
