9d5a32aa4f1e85665355b0dddbefbe2037bb5f50
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-08 06:08:16 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-ionoscloud-object-storage-accesskey/index.ts
+++ src/data-ionoscloud-object-storage-accesskey/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/ionos-cloud/ionoscloud/6.6.6/docs/data-sources/object_storage_accesskey
 // generated from terraform resource schema
 
