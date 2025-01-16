ca55efd43a79d8d8f98da4910b23612d5aba4d84
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-16 12:28:07 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@63a530e

--- src/pg-database/index.ts
+++ src/pg-database/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/ionos-cloud/ionoscloud/6.6.9/docs/resources/pg_database
 // generated from terraform resource schema
 
