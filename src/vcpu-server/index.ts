586b475942a90a4fa8c58107d425ba89e8c24676
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-23 22:06:04 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@42ce033

--- src/vcpu-server/index.ts
+++ src/vcpu-server/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/ionos-cloud/ionoscloud/6.6.9/docs/resources/vcpu_server
 // generated from terraform resource schema
 
